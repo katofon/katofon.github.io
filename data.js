22e2f091113f0929858b72c047c9f5f082123449
Fonova Ekaterina <e.fonova@rambler-co.ru>
2019-11-29 11:48:28 +0300

    content

---
+++ data.js
@@ -0,0 +1,103 @@
+const data = [
+    "<b class='green'>Fonova Ekaterina</b>",
+    "Russian Federation, Moscow",
+    "phone: <b>+7 (920) 674 - 33 - 24</b>",
+    "e-mail: <b>katuysha_f@mail.ru</b></br>",
+    "##breaker##",
+    "<b class='green'>CAREER OBJECTIVE</b>: Get Java/Scala developer position.</br>",
+    "##breaker##",
+    "<b class='green'>KEY SKILLS:</b>",
+    "<li>Development languages: Ruby, ActionScript3, Java",
+    "<li>Frameworks: Ruby On Rails, Sinatra, AngularJS, RSpec",
+    "<li>Database: PostgreSQL, MySQL, Redis, Clickhouse",
+    "<li>Tasks tracker: Jira, Redmine, Asana",
+    "<li>Version control system: GIT",
+    "<li>Tools: CI/CD, Gitlab, Kibana, Grafana, Docker, ssh</br>",
+    "##breaker##",
+    "<b class='green'>PROFESSIONAL EXPERIENCE</b>",
+    [
+        "<b class='yellow'>07.2007 – 06.2010</b>",
+        "<b class='yellow'>Сenter of new information technologies</b>, <br>" +
+        "Ivanovo State Textile Academy<br>" +
+        "technician, engineer since 2008<br>" +
+        "<ul>" +
+        "<li>Web site support<br>" +
+        "<li>Developing web site  «Virtual museum»<br>" +
+        "</ul>"
+    ],
+    [
+        "<b class='yellow'>10.2011 – 10.2012</b>",
+        "<b class='yellow'>MS Group</b>, Ivanovo<br>" +
+        "<b>Developer</b><br>" +
+        "<ul>" +
+        "   <li>Development of the employee's client panel<br>" +
+        "   <li>Support company web sites (Bitrix CMS)<br>" +
+        "</ul>"
+    ],
+    [
+        "<b class='yellow'>05.2013 - 11.2017</b>",
+        "<b class='yellow'>Social Quantum</b>, Ivanovo<br>" +
+        "ActionScript, Ruby developer (game dev) </b><br>" +
+        "<ul>" +
+        "   <li>Support and refactoring old and development new mechanics.<br>" +
+            "Front-end - AS3, back-end - Ruby, admin panel- ruby/sinatra<br>" +
+        "   <li>Review commits, deploying<br>" +
+        "</ul>"
+    ],
+    [
+        "<b class='yellow'>12.2017 - 03.2018</b>",
+        "<b class='yellow'>IKIT Lab</b>, remote<br>" +
+        "<b>Ruby on Rails developer, backend</b><br>" +
+        "<ul>" +
+        "   <li>ICO (Ruby on Rails, RSpec, ActiveAdmin)<br>" +
+        "</ul>"
+    ],
+    [
+        "<b class='yellow'>07.2018 - present</b>",
+        "<b class='yellow'>Rambler Group</b>, Moscow</br>" +
+        "Ruby developer, videoplatform, backend</br>" +
+        "Videoplatform – is a service for uploading video by owners of other web resources with a big traffic (Rambler.ru, Lenta.ru, Afisha.ru, Letidor.ru, Eda.ru).</br>" +
+        "Stack: Ruby, Postgres, Redis, Clickhouse, Docker, Java, Kafka, microservices</b></br>" +
+        "<ul>" +
+        "   <li>Developing, support and refactoring platform (11 micro-services)</br>" +
+        "   <li>Creating new statistics micro-service:</br>" +
+        "   <ul>" +
+        "       <li>getting statistics from other services</br>" +
+        "       <li>preparing statistics and adding to Clickhouse DB using csv</br>" +
+        "   </ul>" +
+        "   <li>Refactor Clickhouse SQL queries, create aggregation db schemas</br>" +
+        "   <li>Prepare metrics for prometheus and creating alerts</br>" +
+        "   <li>Integration with youtube (autentification, upload video)</br>" +
+        "   <li>Integration Redis sentinel</br>" +
+        "   <li>Rspec</br>" +
+        "   <li>Moving project to java language</br>" +
+        "   <li>Creating counting traffic service" +
+        "</ul>"
+    ],
+    "##breaker##",
+    "<b class='green'>EDUCATION</b>",
+    [
+        "2005 – 2010",
+        "<b>Ivanovo State Textile Academy</b></br>" +
+        "Information technology in design</br>" +
+        "Honours degree</br>"
+    ],
+    "##breaker##",
+    "<b class='green'>COURSES AND TRAININGS</b>",
+    [
+        "2007-2010",
+        "<b>Ivanovo State Textile Academy</b></br>" +
+        "Advanced training in in-depth study of the</br> American version of English</br>"
+    ],
+    "##breaker##",
+    "<b class='green'>FOREIGN LANGUAGES</b>",
+    "English – intermediate.",
+    "Work experience in USA during 8 month according program Work & Travel.</br>",
+
+    "##breaker##",
+    "<b class='green'>PERSONAL INFORMATION</b>",
+    "Date of birth: 14.08.1987",
+    "Marital status: single",
+    "Children: no<br>"
+];
+
